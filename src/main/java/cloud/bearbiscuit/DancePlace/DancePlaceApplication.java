dc9f949a9a3d55640a1091481abd450d6a405c48
爱吃小熊饼干 <55008898+BearBiscuit05@users.noreply.github.com>
2021-10-29 19:33:17 +0800

    successful run a mini Springboot program

---
+++ src/main/java/cloud/bearbiscuit/DancePlace/DancePlaceApplication.java
@@ -0,0 +1,18 @@
+package cloud.bearbiscuit.DancePlace;
+
+/**
+ * @version 1.00
+ * @Author BearBiscuit
+ * @Date 2021-10-29
+ * @Description
+ */
+//import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancePlaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DancePlaceApplication.class, args);
+    }
+}
